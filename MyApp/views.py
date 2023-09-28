ede3a27f96a399445448fdba646503a77ef30a90
Bruno Yun <brunoyun@MacBook-Pro-de-Bruno.local>
2023-09-28 14:02:02 +0200

    added tempplate

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,16 +1,11 @@
 # example/views.py
 from datetime import datetime
-
+from django.template import loader
 from django.http import HttpResponse
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello Bruno! v2</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    template = loader.get_template("MyApp/index.html")
+    context = {
+        "athlete": 3,
+    }
+    return HttpResponse(template.render(context, request))
