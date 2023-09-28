2c1a78363f8cff1f8dfa4fc6099d2453fc399e65
Bruno Yun <brunoyun@MacBook-Pro-de-Bruno.local>
2023-09-28 14:56:48 +0200

    added graph

--- MyApp/views.py
+++ MyApp/views.py
@@ -5,7 +5,23 @@ from django.http import HttpResponse
 
 def index(request):
     template = loader.get_template("MyApp/index.html")
-    context = {
-        "athlete": 3,
+
+
+    graph_data = {
+            "nodes": [
+                {"id": 1, "name": "Node 1"},
+                {"id": 2, "name": "Node 2"},
+                # Add more nodes as needed
+            ],
+            "links": [
+                {"source": 1, "target": 2},
+                # Add more links as needed
+            ],
     }
+
+    context = {
+            "athlete": 3,
+            'graph_data': json.dumps(graph_data),
+    }
+
     return HttpResponse(template.render(context, request))
