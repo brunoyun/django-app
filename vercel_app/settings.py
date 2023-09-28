cba1becf74949f1130a5388c93e2e8b7b23a4f43
Bruno Yun <brunoyun@MacBook-Pro-de-Bruno.local>
2023-09-28 14:47:09 +0200

    fix bug static v8

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -119,6 +119,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
