e489744fa32f9dc0d8325fcd7968f7090926e364
brunoyun <bruno.yun@abdn.ac.uk>
2023-10-04 13:02:42 +0200

    converting to DOT

--- MyApp/views.py
+++ MyApp/views.py
@@ -74,19 +74,23 @@ def compute(request):
         arg_dict = { a: i for (i,a) in enumerate(arguments)}
 
         #We create the graph to send to the view
-        gdata_input = {
-            "nodes": [{"id": arg_dict[a], "name": a} for a in arguments],
-            "links": [{"source": arg_dict[a], "target": arg_dict[b]} for (a,b) in attacks],
-        }
+        # gdata_input = {
+        #     "nodes": [{"id": arg_dict[a], "name": a} for a in arguments],
+        #     "links": [{"source": arg_dict[a], "target": arg_dict[b]} for (a,b) in attacks],
+        # }
 
+
+        console_text += "Computing semantics using the semantics: "+selected_semantics+". \n"
         G = nx.DiGraph()
         G.add_nodes_from(range(len(arguments)))
         G.add_edges_from([(arg_dict[a],arg_dict[b]) for (a,b) in attacks])
         set_degrees(G,sem=selected_semantics)
+        set_Shapley_measure(G,sem=selected_semantics)
 
         information_arg = [{"arg": index_dict[i],
                        "degree": G.nodes[i]["degree"]} for i in range(len(arguments))]
 
+        gdata_input = convert_to_dot(G)
 
         response_data = {'console': console_text,
                          'graph_data': gdata_input,
@@ -135,3 +139,66 @@ def set_degrees(G, sem="cat", epsilon=0.0001):
 
     nx.set_node_attributes(G,{list_nodes[i]: deg for (i,deg) in enumerate(current_degrees)}, name="degree")
     return G
+
+
+def copy_graph_without_X(G,X):
+    G2 = nx.DiGraph()
+    for g in G.nodes():
+        G2.add_node(g)
+
+    for (u,v) in G.edges():
+        if(not ((u,v) in X)):
+            G2.add_edge(u,v)
+    return G2
+
+
+def set_Shapley_measure(G, sem):
+    list_edges = list(G.edges())
+    list_intensity = []
+
+
+    for e in list_edges:
+        attackers = [x for x in G.predecessors(e[1])] #We get the list of attackers of the attacked argument
+        n = len(attackers)
+
+        #print("EDGE:",e)
+
+        if (n == 1):
+            #If there is only one attack, the contrib. is 1 - degree
+            list_intensity.append(1-G.nodes[e[1]]["degree"])
+        else:
+            #If there are multiple attacks, we need to consider subsets of attacks
+            #print("attackers",attackers)
+            Y = [ y for y in attackers if y != e[0]]
+            total = 0
+            for X in (list(powerset(Y))):
+                sizeX = len(X)
+                X_edges = [(x, e[1]) for x in X]
+
+                #print("X",X_edges)
+
+                A1 = copy_graph_without_X(G,X_edges)
+                set_degrees(A1, sem)
+                deg_A1 = A1.nodes[e[1]]["degree"]
+
+
+                X_edges.append((e[0],e[1]))
+                A2 = copy_graph_without_X(G,X_edges)
+                set_degrees(A2, sem)
+                #print("A2",A2.nodes(data=True), A2.edges())
+                #print(A2.nodes[e[1]]["degree"])
+                deg_A2 = A2.nodes[e[1]]["degree"]
+
+                total += math.factorial(sizeX) * math.factorial(n-sizeX-1) / math.factorial(n) * (deg_A2-deg_A1)
+            list_intensity.append(total)
+
+    nx.set_edge_attributes(G, {list_edges[i] : list_intensity[i] for i in range(len(list_edges))}, name="attack_intensity")
+
+def convert_to_dot(G):
+    if(not nx.is_empty(G)):
+        result= "digraph G {"
+        for a in G.nodes:
+            result+= str(a)+" [xlabel="+str(round(G.nodes[a]["degree"],3))+"];"
+        for (i,j) in G.edges:
+            result+= str(i)+" -> "+str(j)+" [label="+str(round(G[i][j]["attack_intensity"],3))+"];"
+        return result+"}"
